9c68a08cb9fca2e486166ef6f7c8b91b1d8a1915
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 19:20:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let tempo = Number(prompt("Digite o tempo gasto na viagem"))
+let velocidade = Number(prompt("Velocidade média durante a viagem"))
+let distancia = tempo*velocidade
+let litrosUsados = distancia/12
+alert("A distancia foi "+distancia + "e os litros usados foi "+litrosUsados)
